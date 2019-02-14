4115c8018a8e837f4f534371a098853757b8b3b9
ChaeyunBae <qocodbssla@naver.com>
2019-02-14 10:41:17 +0900

    nohotcorner 추가

--- scripts/nohotcorner.sh
+++ scripts/nohotcorner.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as Root" 1>&2
+	exit 1
+fi
 yum -y install gnome-tweak-tool
 yum -y install gnome-shell-extension-no-hot-corner
-#$reboot
- $gnome-tweaks
+# $reboot을 하고 $gnome-tweaks를 타이핑 후, 확장자 메뉴에서 notopleft hotcorner를 켭니다.
