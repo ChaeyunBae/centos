dacc95617c3a3c242fb6451d7cde63eff7aa775b
ChaeyunBae <qocodbssla@naver.com>
2019-02-26 16:47:04 +0900

    빠진 날 한 거 추가

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#CentOS7.6 에서 사용할 소프트웨어를  자동으로 설치하는 스크립트
+
 if [[ EUID -e 0]]; then
 ./root/nvidia.sh
 ./root/code.sh
@@ -13,7 +15,6 @@ if [[ EUID -e 0]]; then
 ./root/clearcache.sh
 ./root/mongodb.sh
 ./root/openimageio.sh
-
 else
 #install $USER
 ./user/cmake.sh
@@ -23,3 +24,10 @@ else
 ./user/rmbeep.sh
 ./user/trans.sh
 ./user/utility.sh
+#gcc6.x를 이용해서 빌드한다.
+scl enable devtoolset-6 ./user/cmake.sh
+scl enable devtoolset-6 ./user/openexr.sh
+scl enable devtoolset-6 ./user/opencolorio.sh
+scl enable devtoolset-6 ./user/alembic.sh
+./rmbeep.sh
+fi
