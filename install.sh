08b5ba0a5ee0ddaa7237baa2d54643df0f5ff52d
ChaeyunBae <qocodbssla@naver.com>
2019-02-26 16:39:36 +0900

    git 등록 추가

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-echo "source home/$USER/centos/env/init.env" >> ~/.bashrc
-echo "source home/$USER/centos/env/help.env" >> ~/.bashrc
+echo "source home/$USER/centos/env/init.env" >> /home/$USER/.bashrc
+echo "source home/$USER/centos/env/help.env" >> /home/$USER/.bashrc
+
+if [ ! -d "$HOME/bin" ]; then
+	mkdir $HOME/bin
+fi
+cp ./bin/* $HOME/bin
+LANG=C xdg-user-dirs-gtk-update
+
+git config --global user.name "dchecheb"
+git config --global user.email qocodbssla@naver.com
+git config --global color.ui true
